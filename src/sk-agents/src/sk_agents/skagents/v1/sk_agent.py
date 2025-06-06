68dfab9329e41614c240c3b54eb2dec890c5984d
David Daniel <david.daniel@merck.com>
2025-06-06 10:04:37 -0500

    Demo with usage

--- src/sk-agents/src/sk_agents/skagents/v1/sk_agent.py
+++ src/sk-agents/src/sk_agents/skagents/v1/sk_agent.py
@@ -10,6 +10,16 @@ from semantic_kernel.contents.streaming_chat_message_content import (
 
 from sk_agents.ska_types import ModelType
 
+from semantic_kernel.services.ai_service_client_base import AIServiceClientBase
+from semantic_kernel.connectors.ai.open_ai.prompt_execution_settings.azure_chat_prompt_execution_settings import (
+    AzureChatPromptExecutionSettings,
+)
+from semantic_kernel.functions.kernel_arguments import KernelArguments
+
+from semantic_kernel.connectors.ai.open_ai.services.azure_chat_completion import (
+    AzureChatCompletion,
+)
+
 
 class SKAgent:
     def __init__(
@@ -31,8 +41,27 @@ class SKAgent:
     async def invoke_stream(
         self, history: ChatHistory
     ) -> AsyncIterable[StreamingChatMessageContent]:
-        async for result in self.agent.invoke_stream(messages=history):
-            yield result.content
+        service: AzureChatCompletion = self.agent.kernel.get_service(
+            service_id="default"
+        )
+        settings: AzureChatPromptExecutionSettings = (
+            service.get_prompt_execution_settings_class()(service_id="default")
+        )
+        settings.extra_body = {
+            "stream_options": {
+                "include_usage": True,
+            }
+        }
+        async for chunk in service.get_streaming_chat_message_contents(
+            chat_history=history,
+            settings=settings,
+            kernel=self.agent.kernel,
+            arguments=KernelArguments(settings=settings),
+        ):
+            print(chunk[0].metadata["usage"])
+            yield chunk[0]
+        # async for result in self.agent.invoke_stream(messages=history):
+        #     yield result.content
 
     async def invoke(self, history: ChatHistory) -> AsyncIterable[ChatMessageContent]:
         async for result in self.agent.invoke(messages=history):

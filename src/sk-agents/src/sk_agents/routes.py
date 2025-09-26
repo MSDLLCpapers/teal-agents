b4788a04eed9370852337ee6a37b6d8a6e5197b1
Brandon Zander <brandon.zander@merck.com>
2025-09-25 19:10:41 -0500

    Fix linting

--- src/sk-agents/src/sk_agents/routes.py
+++ src/sk-agents/src/sk_agents/routes.py
@@ -37,8 +37,8 @@ from sk_agents.skagents import handle as skagents_handle
 from sk_agents.skagents.chat_completion_builder import ChatCompletionBuilder
 from sk_agents.state import StateManager
 from sk_agents.tealagents.models import ResumeRequest, StateResponse, TaskStatus, UserMessage
-from sk_agents.tealagents.v1alpha1.agent_builder import AgentBuilder
 from sk_agents.tealagents.v1alpha1.agent.handler import TealAgentsV1Alpha1Handler
+from sk_agents.tealagents.v1alpha1.agent_builder import AgentBuilder
 from sk_agents.utils import docstring_parameter, get_sse_event_for_response
 
 logger = logging.getLogger(__name__)
@@ -108,13 +108,13 @@ class Routes:
             ),
             task_store=task_store,
         )
+
     @staticmethod
     def get_task_handler(
-        config: BaseConfig,
-        app_config: AppConfig,
-        agent_builder: AgentBuilder
+        config: BaseConfig, app_config: AppConfig, agent_builder: AgentBuilder
     ) -> TealAgentsV1Alpha1Handler:
         return TealAgentsV1Alpha1Handler()
+
     @staticmethod
     def get_a2a_routes(
         name: str,
@@ -322,10 +322,7 @@ class Routes:
                 # New task
                 session_id, task_id = await state_manager.create_task(message.session_id, user_id)
                 task_state = await state_manager.get_task(task_id)
-                response_content = await TealAgentsV1Alpha1Handler.invoke(
-                    user_id,
-                    message
-                )
+                response_content = await TealAgentsV1Alpha1Handler.invoke(user_id, message)
             else:
                 # Follow-on request
                 task_id = message.task_id

9fcc8d146bd1d1dcff0637ea72179852ad6edeec
Brandon Zander <brandon.zander@merck.com>
2025-09-30 13:59:43 -0500

    Test singleton update

--- src/sk-agents/src/sk_agents/authorization/authorizer_factory.py
+++ src/sk-agents/src/sk_agents/authorization/authorizer_factory.py
@@ -1,9 +1,8 @@
-from ska_utils import AppConfig, ModuleLoader
+from ska_utils import AppConfig, ModuleLoader, Singleton
 
 from sk_agents.configs import TA_AUTHORIZER_CLASS, TA_AUTHORIZER_MODULE
 
 from .request_authorizer import RequestAuthorizer
-from .singleton import Singleton as Singleton
 
 
 class AuthorizerFactory(metaclass=Singleton):

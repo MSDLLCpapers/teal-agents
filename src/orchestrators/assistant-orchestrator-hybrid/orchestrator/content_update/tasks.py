7d815897adeaaf5f3b04d3406afc080789d6f19c
sinhauts <utsav.sinha@merck.com>
2026-02-06 17:40:45 +0530

    pushing lateste clery changes

--- src/orchestrators/assistant-orchestrator-hybrid/orchestrator/content_update/tasks.py
+++ src/orchestrators/assistant-orchestrator-hybrid/orchestrator/content_update/tasks.py
@@ -1,8 +1,12 @@
 from content_update.celery_app import celery_app
 from content_update.chroma_client import get_collection
+from content_update.tfidf_service import TfidfLearningService
+
 
 import logging
 logger = logging.getLogger(__name__)
+
+tfidf_service = TfidfLearningService()
 
 @celery_app.task
 def dummy_task(a, b):
@@ -11,18 +15,20 @@ def dummy_task(a, b):
     print(f"Running dummy task: {a} + {b}")
     return a + b
 
-
 @celery_app.task(bind=True)
-def update_metadata(self, agent_name, key_word_list):
+def update_metadata(self, agent_name, agent_response):
     logger.info("Started metadata update for %s", agent_name)
-    logger.info("kw list %s", key_word_list)
+    logger.info("kw list %s", agent_response)
 
     collection = get_collection()
-
+    logger.info("....gathering agent details....")
+    agent_info = tfidf_service.get_agent_details(collection,agent_name)
+    new_keywords ,updated_key_words= tfidf_service.learn_keywords(agent_info, agent_name, agent_response)
+    logger.info(".....new learned kewords...."+str(new_keywords))
     try:
         collection.update(
             ids=[agent_name],
-            metadatas=[{"desc_keywords": key_word_list}]
+            metadatas=[{"desc_keywords": ",".join(updated_key_words)}]
         )
     except Exception:
         logger.exception("Chroma update failed")
@@ -30,6 +36,27 @@ def update_metadata(self, agent_name, key_word_list):
 
     logger.warning("Metadata update successful for %s", agent_name)
     return True
+
+
+# @celery_app.task(bind=True)
+# def update_metadata(self, agent_name, key_word_list):
+#     logger.info("Started metadata update for %s", agent_name)
+#     logger.info("kw list %s", key_word_list)
+
+#     collection = get_collection()
+
+
+#     try:
+#         collection.update(
+#             ids=[agent_name],
+#             metadatas=[{"desc_keywords": key_word_list}]
+#         )
+#     except Exception:
+#         logger.exception("Chroma update failed")
+#         raise
+
+#     logger.warning("Metadata update successful for %s", agent_name)
+#     return True
     
     
 
